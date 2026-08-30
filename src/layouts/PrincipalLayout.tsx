70ac731c461d3fc5df7fb3943e177410b5c36ad1
Alex Pacheco <alexpacheco407@gmail.com>
2026-08-30 14:04:21 -0400

    FE-14: feat(FloatingButtons): desarrollo de componente FloatingButtons

--- src/layouts/PrincipalLayout.tsx
+++ src/layouts/PrincipalLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "@tanstack/react-router";
 import { Header } from "../components/layout/Header";
 import { Footer } from "../components/layout/Footer";
+import { FloatingButtons } from "../components/layout/FloatingButtons";
 
 export const PrincipalLayout = () => {
     return (
@@ -10,6 +11,7 @@ export const PrincipalLayout = () => {
                 <Outlet />
             </main>
             <Footer />
+            <FloatingButtons />
         </div>
     );
 };
