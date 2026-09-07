017d29b22a2c7dd91fe3767cdc022d2e19f3ce7c
Alex Pacheco <alexpacheco407@gmail.com>
2026-09-07 16:55:32 -0400

    FE-23: feat(AboutUs): aliminacion de importacion que no se esta utilizando

--- src/components/HomeScreen/AboutUs.tsx
+++ src/components/HomeScreen/AboutUs.tsx
@@ -1,8 +1,6 @@
 import { useTranslation } from "react-i18next";
 import type { IconType } from "react-icons";
 import {
-    FaArrowDown,
-    FaArrowRight,
     FaBriefcase,
     FaGraduationCap,
     FaHandshake,
